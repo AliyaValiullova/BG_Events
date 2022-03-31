ae78d1de87c29b278ea0ad02dedb3158eeef96bb
Aliya572 <aliavaliullova2013@yandex.ru>
2022-03-31 10:58:15 +0300

    Добавьте файлы проекта.

---
+++ BG_Events/Program.cs
@@ -0,0 +1,35 @@
+﻿using  System;
+namespace BG_Events
+
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+            Work work = new Work();
+            work.OnKeyProssed += Work_OnKeyPressed;
+            Console.WriteLine("Для завершения программы нажмите C");
+            Console.WriteLine("Введите символ");
+            work.Run();
+        }
+
+        private static void Work_OnKeyPressed(Object sender, char x)
+        { Console.WriteLine( $"{x}");
+        }
+}
+    class Work
+    {
+        public event EventHandler <char>? OnKeyProssed;
+        ConsoleKeyInfo w;
+        public void Run()
+        {
+            while(Console.ReadKey().Key !=ConsoleKey.C)
+            {  w = Console.ReadKey();
+                char x = w.KeyChar;
+                OnKeyProssed?.Invoke(this, x);
+            }
+        }
+         
+
+    }
+}
